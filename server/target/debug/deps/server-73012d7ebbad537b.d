cce3f26c7d60c921ec3af6af27cef47816dac878
Your Name <you@example.com>
2024-08-21 09:04:10 +0200

    starting server

--- server/target/debug/deps/server-73012d7ebbad537b.d
+++
@@ -1,5 +0,0 @@
-/home/victor/git/metroid/server/target/debug/deps/libserver-73012d7ebbad537b.rmeta: src/main.rs
-
-/home/victor/git/metroid/server/target/debug/deps/server-73012d7ebbad537b.d: src/main.rs
-
-src/main.rs:
--- server/target/debug/server.d
+++
@@ -1,1 +0,0 @@
-/home/victor/git/metroid/server/target/debug/server: /home/victor/git/metroid/server/src/main.rs
